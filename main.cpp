ef9784fcec4657a6debc12e0d28ee68b942fe111
Innokentiy Filippov <innokentiy.filippov@hexagon.com>
2021-08-30 15:31:09 +0700

    Added build number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#ifdef GITHUB_RUN_NUMBER
+constexpr int version = GITHUB_RUN_NUMBER;
+#else
+constexpr int version = -1;
+#endif
+
 int main()
 {
+    std::cout << "Bulid: " << version << std::endl;
     std::cout << "Hello World!";
+    system("pause");
     return 0;
 }
